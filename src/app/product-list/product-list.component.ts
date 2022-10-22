66446c89df88750e52d2e9577565b89acd0fb8e5
Agustin Pieres <agustincasita@gmail.com>
2022-10-22 19:31:56 -0300

    Crear funciones, botones y entitys de productos

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'product-list',
@@ -7,17 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  product = {
+  products : Product[] = [{
     type: "Camiseta",
     name: "Authentic Titular Boca Jrs 22/23",
     waist: "M",
     color: "Azul y Oro",
-    price: 28999,
+    stock: 400,
+    price: 28999.99,
     image: "assets/camiseta-boca.png",
-  }
+    clearance: false,
+    quantity: 0,
+  },
+  {
+    type: "Camiseta",
+    name: "CAMISETA PUMA TITULAR INDEPENDIENTE 2022/23",
+    waist: "XL",
+    color: "Rojo",
+    stock: 350,
+    price: 15999.99,
+    image: "assets/camiseta-independiente.png",
+    clearance: true,
+    quantity: 0,
+  },{
+    type: "Camiseta",
+    name: "CAMISETA TITULAR RIVER PLATE 22/23",
+    waist: "2XL",
+    color: "Blanco y Rojo",
+    stock: 300,
+    price: 16999.99,
+    image: "assets/camiseta-river.png",
+    clearance: false,
+    quantity: 0,
+  }]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  upQuantity(product: Product): void{
+    if(product.stock > product.quantity){
+      product.quantity++;
+    }
+  }
+  downQuantity(product: Product): void{
+    if(product.quantity > 0){
+      product.quantity--;
+    }  
+  }
+  verifyProductsQuantity(product: Product): void{
+    if(product.stock < product.quantity){
+      alert("No se pueden ordenar más productos de los que hay en stock");
+    }
+
+    if(product.quantity < 0 ){
+      alert("No se pueden pedir menos que 0 productos");
+      product.quantity = 0;
+    }
+  }
 }
---
+++ src/app/product-list/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    name: string;
+    type: string;
+    waist: string;
+    color: string;
+    stock: number;
+    price: number;
+    image: string;
+    quantity: number;
+    clearance: boolean;
+}
