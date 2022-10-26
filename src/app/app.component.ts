e946d935b7c43dde0390459b9f7b54ac7bd859bb
Agustin Pieres <agustincasita@gmail.com>
2022-10-26 19:20:02 -0300

    Incluir componentes nuevos en App.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  year = new Date().getFullYear()
-  title = 'prueba-angular';
+  title = 'SoloSports';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     CartComponent,
+    FooterComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
