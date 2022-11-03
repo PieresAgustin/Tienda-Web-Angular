a9ce45e74c404c21788de0181717c0ba5015238c
Agustín Pieres <pieresagustin@gmail.com>
2022-11-02 22:48:43 -0300

    Agregar rutas de la NavBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: 'products',pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'about', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
