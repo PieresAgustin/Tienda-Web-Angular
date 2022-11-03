930d61f7cd396d33bb3950d4788a6b1d808c7ce9
Agustín Pieres <pieresagustin@gmail.com>
2022-11-02 22:46:28 -0300

    Agregar Modulos nuevos a AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CartComponent } from './cart/cart.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsComponent } from './products/products.component'
+import { ProductListComponent } from './product-list/product-list.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    CartComponent,
     FooterComponent,
     NavBarComponent,
+    ProductsComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
