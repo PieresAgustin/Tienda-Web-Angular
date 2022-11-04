3edfc17b888b30df86e304fd4c03d0658f5f7677
Agustín Pieres <pieresagustin@gmail.com>
2022-11-03 21:05:38 -0300

    Agregar createProduct en products-data

--- src/app/products-data.service.ts
+++ src/app/products-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './product-list/product';
@@ -9,9 +9,20 @@ const URL = "http://localhost:3000/products";
 })
 export class ProductsDataService {
 
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Product[]>{
     return this.http.get<Product[]>(URL);
+  }
+
+  createProduct(product: any):Observable<Product> {
+    //console.log(product);
+    return this.http.post<any>(URL,product,this.httpOptions);
   }
 }
