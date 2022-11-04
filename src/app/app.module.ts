8642a24ab29d077da47689a5c4dd1c77d44890a6
Agustín Pieres <pieresagustin@gmail.com>
2022-11-03 21:11:44 -0300

    Agregar AddProductComponent en AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
-import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProductsComponent } from './products/products.component'
 import { ProductListComponent } from './product-list/product-list.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CartComponent } from './cart/cart.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    FooterComponent,
     NavBarComponent,
     ProductsComponent,
     AboutUsComponent,
     CartComponent,
+    AddProductComponent,
   ],
   imports: [
     BrowserModule,
