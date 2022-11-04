98095d07250b8cbfb6bd95f9825554d0987ed384
Agustín Pieres <pieresagustin@gmail.com>
2022-11-03 21:08:57 -0300

    Crear ruta a New Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '',redirectTo: 'products',pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
   { path: 'about', component: AboutUsComponent },
+  { path: 'new', component: AddProductComponent},
 ];
 
 @NgModule({
