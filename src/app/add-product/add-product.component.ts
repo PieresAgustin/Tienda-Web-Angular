e3a3b343ea770498477990d8cb515275107cae2c
Agustín Pieres <pieresagustin@gmail.com>
2022-11-17 13:50:32 -0300

    Crear AddProductComponent

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Brand } from '../brand';
 import { BrandDataService } from '../brand-data.service';
 import { ProductsDataService } from '../products-data.service';
-import { Product } from './product';
+import { Product } from '../product';
 
 
 @Component({
@@ -14,8 +14,22 @@ export class AddProductComponent implements OnInit {
 
   brands:Brand[] = [];
 
-  product = new Product("camiseta titular","XL","Camiseta","Blanco",200,17000,"assets/camiseta-river.png",0,false);
-
+  product:Product = {
+    id: 0,
+    name: "",
+    type: "",
+    waist: "",
+    color: "",
+    stock: 0,
+    price: 0,
+    image: "",
+    quantity: 0,
+    clearance: false,
+    brand: {
+      brand_id: 0,
+      marca: "",
+    }
+  }
   constructor(private productsDataService: ProductsDataService, private brandsDataService: BrandDataService) { }
 
   ngOnInit(): void {
@@ -24,7 +38,16 @@ export class AddProductComponent implements OnInit {
   }
 
   addProduct():void{
-    this.productsDataService.createProduct(this.product).subscribe(product => this.product = product)
+    this.productsDataService.createProduct(this.product).subscribe(product => {
+      this.product.name = product.name
+      this.product.color = product.color
+      this.product.price = product.price
+      this.product.stock = product.stock
+      this.product.type = product.type
+      this.product.waist = product.waist
+      this.product.clearance = product.clearance
+      this.product.brand = product.brand
+    })
   }
 
 }
