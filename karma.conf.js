424391075f68ce578618cac382a54eeeb5c46778
Agustin Pieres <pieresagustin@gmail.com>
2023-09-14 19:17:00 -0300

    Update dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/prueba-angular'),
+      dir: require('path').join(__dirname, './coverage/tienda-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
