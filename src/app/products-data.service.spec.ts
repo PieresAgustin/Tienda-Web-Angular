3c309327ad9796b7383c9fa72e9cb5acd6f4be7e
Agustin Pieres <pieresagustin@gmail.com>
2023-10-05 18:31:43 -0300

    Add HttpClientTestingModule in imports

--- src/app/products-data.service.spec.ts
+++ src/app/products-data.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsDataService } from './products-data.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProductsDataService', () => {
   let service: ProductsDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProductsDataService);
   });
 
